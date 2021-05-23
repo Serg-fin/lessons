817f5c0083a46b0eeff4135e75b34c2925f84b33
Sergey Finageev <safary2007.2@gmail.com>
2021-05-23 18:35:12 +0300

    Level2: lesson1 + HomeWork

---
+++ Level_2/src/lesson1/Cat.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class Cat {
+    public static void main(String[] args) {
+        canRun();
+        canJump();
+    }
+    private static void canRun() {
+        System.out.println("Кот может бегать.");
+    }
+    private static void canJump() {
+        System.out.println("Кот может прыгать.");
+    }
+}
---
+++ Level_2/src/lesson1/Human.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class Human {
+    public static void main(String[] args) {
+        canRun();
+        canJump();
+    }
+    private static void canRun() {
+        System.out.println("Человек может бегать.");
+    }
+    private static void canJump() {
+        System.out.println("Человек может прыгать.");
+    }
+}
+
